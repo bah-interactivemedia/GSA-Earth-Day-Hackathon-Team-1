cf2c009650ee8d5d599df9ad48b7a4f1a5a8d1f4
Kevin Li <li_kevin@bah.com>
2016-04-22 11:53:59 -0400

    React

---
+++ public/js/src/app.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var AppComponent = require('./components/AppComponent.jsx');
+
+ReactDOM.render(
+	  <AppComponent />,
+	  document.getElementById('app')
+	);
---
+++ public/js/src/components/AppComponent.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class AppComponent extends React.Component {
+	render() {
+		return (
+			<div>
+				Test
+			</div>
+		);
+	}
+}
+
+module.exports = AppComponent;
