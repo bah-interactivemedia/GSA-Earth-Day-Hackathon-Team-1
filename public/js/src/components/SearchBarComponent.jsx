cf43c1485eb6bed1daa5c9fcba20997f5693ac81
Elizabeth Dabbs <dabbs_elizabeth@bah.com>
2016-04-22 12:27:45 -0400

    styling

--- public/js/src/components/SearchBarComponent.jsx
+++ public/js/src/components/SearchBarComponent.jsx
@@ -4,16 +4,18 @@ class SearchBar extends React.Component {
 	render() {
 		return (
 			<div className="row">
-	            <div className="serach-bar">
-	                <div className="input-group">
-	                    <input type="text" className="form-control" placeholder="Enter Address" />
+				<div className="col-xs-12">
+					<div className="serach-bar">
+						<div className="input-group">
+							<input type="text" className="form-control" placeholder="Enter Address" />
 	                    <span className="input-group-btn">
 	                        <button className="btn btn-default" type="button">
-	                                <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
-	                        </button>
+								<span className="glyphicon glyphicon-search" aria-hidden="true"></span>
+							</button>
 	                    </span>
-	                </div>
-	            </div>
+						</div>
+					</div>
+				</div>
 	        </div>
 		);
 	}
